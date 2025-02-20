c6b49ef172ee31fa3f6ebe18f8af71ab493d5df5
BlueBerrySuS <137744311+BlueBerrySuS@users.noreply.github.com>
2025-02-20 16:12:42 +0600

    init

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes;
