b21ca03c0310654eb8c5d3c1270a07a751fb3627
BlueBerrySuS <137744311+BlueBerrySuS@users.noreply.github.com>
2025-02-23 15:14:55 +0600

    Implement firebase db connect

---
+++ src/api/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOrBXVnkAJgkgh65lWaIDnSUflEAWLjp8",
+  authDomain: "omni-store-3a120.firebaseapp.com",
+  projectId: "omni-store-3a120",
+  storageBucket: "omni-store-3a120.firebasestorage.app",
+  messagingSenderId: "796622699745",
+  appId: "1:796622699745:web:1ae57c52ac40ababdd0ba0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
