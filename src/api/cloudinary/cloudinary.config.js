3730e7f644b534ac5b5937e3d18e606ee390e3c4
BlueBerrySuS <137744311+BlueBerrySuS@users.noreply.github.com>
2025-03-03 17:23:39 +0600

    Add cloudinary config

---
+++ src/api/cloudinary/cloudinary.config.js
@@ -0,0 +1,11 @@
+
+
+const cloudinaryConfig = {
+    cloud_name: 'djl0ohbf2', 
+    api_key: '929574991528345',
+    upload_presets: {
+        products: "products_imgs",
+    },
+  };
+  
+export default cloudinaryConfig;
