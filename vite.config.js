dddd5d6662546945678cf32051152524e92abf43
BlueBerrySuS <137744311+BlueBerrySuS@users.noreply.github.com>
2025-03-26 09:49:46 +0600

    Added Vercel and Tailwind plugins to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vercel from 'vite-plugin-vercel';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(),vercel(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000"
+    }
+  }
+});
+
